8c40a0e7d4aa428f0a291d10f982dc61b836140e
celularesradical-commits <celularesradical@gmail.com>
2026-08-05 00:09:19 -0300

    Criar novo_reparo.py

---
+++ pages/novo_reparo.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Nova Ordem de Serviço",
+    page_icon="🔧",
+    layout="centered"
+)
+
+st.title("🔧 Nova Ordem de Serviço")
+
+st.info("Em desenvolvimento...")
