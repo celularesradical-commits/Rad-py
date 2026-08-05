787a0d3a0e3d862d2cf4034a250161772f28125e
celularesradical-commits <celularesradical@gmail.com>
2026-08-05 00:20:03 -0300

    Criar configuracoes.py

---
+++ pages/configuracoes.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Configurações",
+    page_icon="⚙️",
+    layout="centered"
+)
+
+st.title("⚙️ Configurações")
+
+st.info("Em desenvolvimento.")
