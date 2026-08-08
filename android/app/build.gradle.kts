8546a8cbdaf6f4a2b7af041dadefd5fb82a39d5f
celularesradical-commits <celularesradical@gmail.com>
2026-08-08 13:35:34 -0300

    Criar build.gradle.kts

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.radicalcelulares.radicalsystem"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.radicalcelulares.radicalsystem"
+        minSdk = 24
+        targetSdk = 36
+
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
